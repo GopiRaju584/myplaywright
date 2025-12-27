007b39913b02b76556d629b361d1adb58db84066
GopiRaju584 <your-email@example.com>
2025-12-27 20:47:31 +0530

    commited to changes in git

---
+++ java script/conditional.js
@@ -0,0 +1,19 @@
+let x=10;
+if (x > 5) {
+  console.log("x is greater than 5");
+}
+
+if (x > 5){
+    print("x is greater than 5")
+}else{
+    print("x is not greater than 5")
+}
+
+if (score >= 90) {
+    System.out.println("Grade A");
+} else if (score >= 75) {
+    System.out.println("Grade B");
+} else {
+    System.out.println("Grade C");
+}
+
